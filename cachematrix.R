0111b9e1544d479642b8d52a4a679e61176ebb88
magnar-n <magnar@naustdalslid.com>
2015-10-25 20:40:59 +0100

    Second commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions assign a value to a "matrix 
+## object" in the cached memory, i.e. outside the current
+## environment.  
 
-## Write a short comment describing this function
+## The first function creates the "matrix object" and 
+## sets the initial value of that matrix object as NULL.
+## It then gets the value of the matrix; 
+## sets the value of the matrix;
+## sets the value of the inverse of the matrix; 
+## and gets the value of the inverse matrix.  
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -17,8 +23,11 @@ makeCacheMatrix <- function(x = matrix()) {
              getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The second function calculates the inverse of the 
+## matrix object created by the first function. If a 
+## value of the inverse has previously been calculated
+## it gets that value from the cache, otherwise it 
+## calculates the value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
